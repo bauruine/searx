396f537d9f0e513d80885f05c08a1b7ab87d8717
Stefan Spühler <git@tuxli.ch>
2020-05-29 17:19:27 +0200

    Change branding urls

--- searx/brand.py
+++ searx/brand.py
@@ -1,5 +1,6 @@
-GIT_URL = 'https://github.com/asciimoo/searx'
+GIT_URL = 'https://github.com/bauruine/searx/'
 ISSUE_URL = 'https://github.com/asciimoo/searx/issues'
-SEARX_URL = 'https://searx.me'
+SEARX_URL = 'https://searx.info'
 DOCS_URL = 'https://asciimoo.github.io/searx'
 PUBLIC_INSTANCES = 'https://searx.space'
+
